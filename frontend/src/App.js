c8c4d40532b9c1d83260de03fa6339ffe0073150
Arefi6 <arefinhossain3@gmail.com>
2020-12-10 14:16:59 +0600

    Product screen done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import * as ReactBootstrap from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import {Container} from 'react-bootstrap';
 import './index.css';
 import Home from './components/screens/HomeScreen/Home';
+import ProductScreen from './components/screens/ProsuctScreen/ProductScreen';
 
 const App = () => {
   return (
     <>
+    <Router>
       <Header></Header>
       <main className="py-3">
         <Container>
-          <Home></Home>
+          
+           <Switch>
+            <Route path="/" exact>
+            <Home></Home>
+            </Route>
+            <Route  exact path="/product/:id" >
+              <ProductScreen />
+            </Route>
+             
+           </Switch>
+        
         </Container>
       </main>
       <Footer></Footer>
+      </Router>
     </>
   );
 }
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+  
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
